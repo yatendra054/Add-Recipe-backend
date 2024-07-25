b078f8e07c78621803728459d42983604db95743
yatendra054 <yatendrapachori54@gmail.com>
2024-07-25 22:28:13 +0530

    small changes

--- Start/settings.py
+++ Start/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR,"saticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 STATICFILES_DIR = (
     os.path.join(BASE_DIR,"public/static")
--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 
 echo "BUILD START"
-python3.12 -m pip install -r requirement.txt
-python3.12 manage.py collectstatic --noinput --clear
+python3.11.9 -m pip install -r requirement.txt
+python3.11.9 manage.py collectstatic --noinput --clear
 exho "BUILD END"
